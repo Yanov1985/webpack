bfe4f6c7e9aa0e9efc60afbf2277cc600c224dc4
HardyGuardy <yanov.kirill@gmail.com>
2020-07-24 15:21:45 +0300

    patterns

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js',
+		analytics: './src/analytics.js'
+
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist')
+
+	},
+	 plugins: [
+	 new HtmlWebpackPlugin({
+	 	template: './src/index.html'
+
+	 })
+
+	 ], // конец
+};
+
+
+
+
